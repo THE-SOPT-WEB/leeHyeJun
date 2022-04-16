2fb6575c9f8bea14b49db7754e32858eccf0ac20
HyeJun <solar3070@naver.com>
2022-04-16 18:47:33 +0900

    feat: 장바구니 아이템 수량 증가

--- week1/main.js
+++ week1/main.js
@@ -1,19 +1,24 @@
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
-function setElements(burger) {
+let burgerList = [];
+
+function setElements(burgerName, burgerPrice) {
   const tagName = ["span", "input", "span", "button"];
-  const elems = [];
+  let elems = [];
 
   tagName.forEach((name, index) => {
     elems[index] = document.createElement(name);
   });
 
-  elems[0].innerText = burger.querySelector(".burger__name").innerText;
+  elems[0].innerText = burgerName;
   elems[1].setAttribute("value", 1);
-  elems[2].innerText = burger.querySelector(".burger__price").innerText;
+  elems[1].classList.add(`cart__list--${burgerName}-value`);
+  elems[2].innerText = burgerPrice;
   elems[3].setAttribute("type", "button");
   elems[3].innerText = "❌";
+
+  burgerList.push(burgerName);
 
   return elems;
 }
@@ -29,7 +34,13 @@ function addCartItem(cartList, elems) {
 function attachEvent({ cartList, burgerCard }) {
   burgerCard.forEach((burger) => {
     burger.addEventListener("click", () => {
-      addCartItem(cartList, setElements(burger));
+      const burgerName = burger.querySelector(".burger__name").innerText;
+      if (burgerList.includes(burgerName)) {
+        $(`.cart__list--${burgerName}-value`).value++;
+      } else {
+        const burgerPrice = burger.querySelector(".burger__price").innerText;
+        addCartItem(cartList, setElements(burgerName, burgerPrice));
+      }
     });
   });
 }
