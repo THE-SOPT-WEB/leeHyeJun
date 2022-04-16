78a053ae701fc40535e2e4361115bb1f63de6246
HyeJun <solar3070@naver.com>
2022-04-16 17:13:04 +0900

    feat: 버거 카드 클릭시 장바구니 추가

--- week1/main.js
+++ week1/main.js
@@ -1,6 +1,46 @@
-import './style.css'
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function setElements(burger) {
+  const tagName = ["span", "input", "span", "button"];
+  const elems = [];
+
+  tagName.forEach((name, index) => {
+    elems[index] = document.createElement(name);
+  });
+
+  elems[0].innerText = burger.querySelector(".burger__name").innerText;
+  elems[1].setAttribute("value", 1);
+  elems[2].innerText = burger.querySelector(".burger__price").innerText;
+  elems[3].setAttribute("type", "button");
+  elems[3].innerText = "❌";
+
+  return elems;
+}
+
+function addCartItem(cartList, elems) {
+  const item = document.createElement("li");
+  elems.forEach((elem) => {
+    item.appendChild(elem);
+  });
+  cartList.appendChild(item);
+}
+
+function attachEvent({ cartList, burgerCard }) {
+  burgerCard.forEach((burger) => {
+    burger.addEventListener("click", () => {
+      addCartItem(cartList, setElements(burger));
+    });
+  });
+}
+
+function cartManager(cartInfo) {
+  attachEvent(cartInfo);
+}
+
+window.onload = () => {
+  cartManager({
+    cartList: $("ul.cart__list"),
+    burgerCard: $$(".burger__card"),
+  });
+};
