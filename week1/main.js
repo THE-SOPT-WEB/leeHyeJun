157124e70c0a5d7091e5a21b377bffbd41478b59
HyeJun <solar3070@naver.com>
2022-04-16 23:51:08 +0900

    refactor: main.js 리팩토링

--- week1/main.js
+++ week1/main.js
@@ -3,43 +3,46 @@ const $$ = (selector) => document.querySelectorAll(selector);
 
 let burgerList = [];
 
-function setElements(burgerName, burgerPrice) {
+function addCartItem(cartList, burgerName, burgerPrice) {
   const tagName = ["span", "input", "span", "button"];
-  let elems = [];
-
+  const elems = [];
   tagName.forEach((name, index) => {
     elems[index] = document.createElement(name);
   });
 
   elems[0].innerText = burgerName;
+  elems[0].classList.add(`cart__list--${burgerName}`);
   elems[1].setAttribute("value", 1);
-  elems[1].classList.add(`cart__list--${burgerName}-value`);
+  elems[1].classList.add(`cart__list--${burgerName}-qty`);
   elems[2].innerText = burgerPrice;
-  elems[3].setAttribute("type", "button");
+  elems[2].classList.add(`cart__list--${burgerName}-price`);
   elems[3].innerText = "❌";
+  elems[3].setAttribute("type", "button");
+  elems[3].classList.add(`cart__list--${burgerName}-del`);
 
-  burgerList.push(burgerName);
-
-  return elems;
+  const item = document.createElement("li");
+  elems.forEach((elem) => item.appendChild(elem));
+  cartList.appendChild(item);
 }
 
-function addCartItem(cartList, elems) {
-  const item = document.createElement("li");
-  elems.forEach((elem) => {
-    item.appendChild(elem);
-  });
-  cartList.appendChild(item);
+function isInCart(cartList, burgerName) {
+  const children = cartList.childNodes;
+  for (let i = 0; i < children.length; i++)
+    if (burgerName === children[i].firstChild.innerText) return true;
+  return false;
 }
 
 function attachEvent({ cartList, burgerCard }) {
   burgerCard.forEach((burger) => {
     burger.addEventListener("click", () => {
       const burgerName = burger.querySelector(".burger__name").innerText;
-      if (burgerList.includes(burgerName)) {
-        $(`.cart__list--${burgerName}-value`).value++;
-      } else {
-        const burgerPrice = burger.querySelector(".burger__price").innerText;
-        addCartItem(cartList, setElements(burgerName, burgerPrice));
+      const burgerPrice = burger.querySelector(".burger__price").innerText;
+
+      if (isInCart(cartList, burgerName))
+        $(`.cart__list--${burgerName}-qty`).value++;
+      else {
+        burgerList.push(burgerName);
+        addCartItem(cartList, burgerName, burgerPrice);
       }
     });
   });
