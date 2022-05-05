ef5a76adcda7a0350e05cac105469816ea479fc3
HyeJun <solar3070@naver.com>
2022-05-06 03:44:24 +0900

    chore: 게임 데이터 추가

---
+++ week3/src/utils/gameData.js
@@ -0,0 +1,45 @@
+import pic1 from "../assets/닦달여우.gif";
+import pic2 from "../assets/동글여우.gif";
+import pic3 from "../assets/뚱땅여우.gif";
+import pic4 from "../assets/모찌여우.gif";
+import pic5 from "../assets/바보여우.gif";
+import pic6 from "../assets/방전여우.gif";
+import pic7 from "../assets/응앜여우.gif";
+import pic8 from "../assets/풀먹여우.gif";
+
+const gameData = [
+  {
+    img: pic1,
+    name: "닦달여우",
+  },
+  {
+    img: pic2,
+    name: "동글여우",
+  },
+  {
+    img: pic3,
+    name: "뚱땅여우",
+  },
+  {
+    img: pic4,
+    name: "모찌여우",
+  },
+  {
+    img: pic5,
+    name: "바보여우",
+  },
+  {
+    img: pic6,
+    name: "방전여우",
+  },
+  {
+    img: pic7,
+    name: "응앜여우",
+  },
+  {
+    img: pic8,
+    name: "풀먹여우",
+  },
+];
+
+export default gameData;
