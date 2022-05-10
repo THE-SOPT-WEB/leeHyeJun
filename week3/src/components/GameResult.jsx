705e773ffceba298d37e126682ae1f98e35fdbec
HyeJun <solar3070@naver.com>
2022-05-11 01:01:23 +0900

    refactor: 다시하기 버튼 styled-components 위치 변경

--- week3/src/components/GameResult.jsx
+++ week3/src/components/GameResult.jsx
@@ -1,7 +1,6 @@
 import GameItem from "./GameItem";
 import vs from "../assets/vs.png";
 import crown from "../assets/crown.png";
-import styled from "styled-components";
 
 function GameResult({ win, winners, fighters, handleClick }) {
   switch (win) {
@@ -29,13 +28,3 @@ function GameResult({ win, winners, fighters, handleClick }) {
 }
 
 export default GameResult;
-
-const ReBtn = styled.button`
-  background-color: transparent;
-  background-color: #ffdf43;
-  border: none;
-  cursor: pointer;
-  font-size: 40px;
-  color: #ffdf43;
-  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
-`;
