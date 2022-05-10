0bddfaab7fe8d994a02877d8491bbd9cbe610057
HyeJun <solar3070@naver.com>
2022-05-11 03:54:30 +0900

    refactor: GameResult 로직 switch-case문에서 if문으로 변경

--- week3/src/components/GameResult.jsx
+++ week3/src/components/GameResult.jsx
@@ -3,27 +3,26 @@ import vs from "../assets/vs.png";
 import crown from "../assets/crown.png";
 
 function GameResult({ win, winners, fighters, handleClick }) {
-  switch (win) {
-    case true:
-      return (
-        <>
-          <GameItem gameInfo={winners.current[0]} handleClick={handleClick} />
-          <img src={crown} alt="crown" />
-        </>
-      );
-    default:
-      return (
-        <>
-          {fighters.slice(0, 2).map((fighter) => (
-            <GameItem
-              gameInfo={fighter}
-              handleClick={handleClick}
-              key={fighter.name}
-            />
-          ))}
-          <img src={vs} alt="vs" />
-        </>
-      );
+  if (win) {
+    return (
+      <>
+        <GameItem gameInfo={winners.current[0]} handleClick={handleClick} />
+        <img src={crown} alt="crown" />
+      </>
+    );
+  } else {
+    return (
+      <>
+        {fighters.slice(0, 2).map((fighter) => (
+          <GameItem
+            gameInfo={fighter}
+            handleClick={handleClick}
+            key={fighter.name}
+          />
+        ))}
+        <img src={vs} alt="vs" />
+      </>
+    );
   }
 }
 
