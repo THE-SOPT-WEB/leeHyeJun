f6451ff2f120265f42a748da57c3b0a9c4b3e5f4
HyeJun <solar3070@naver.com>
2022-06-10 03:43:59 +0900

    design: GlobalStyle에 폰트 추가

--- week7/src/styles/global.js
+++ week7/src/styles/global.js
@@ -13,6 +13,13 @@ const GlobalStyle = createGlobalStyle`
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/SDSamliphopangche_Outline.woff') format('woff');
     font-weight: normal;
     font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'IM_Hyemin-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2106@1.1/IM_Hyemin-Regular.woff2') format('woff');
+    font-weight: normal;
+    font-style: normal;
 }
 `;
 
