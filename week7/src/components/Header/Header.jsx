fadc64578b6e7b100b04eadf98527df7a1771440
HyeJun <solar3070@naver.com>
2022-06-10 03:45:58 +0900

    feat: Header 추가

---
+++ week7/src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import icon from "../../assets/web_icon.png";
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <img src={icon} alt="웹 아이콘" />
+      <h1>웹파트 우체통</h1>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin-bottom: 40px;
+  padding: 30px;
+  background-color: ${({ theme }) => theme.color.ghostwhite};
+
+  img {
+    width: 40px;
+  }
+
+  h1 {
+    font-family: "SDSamliphopangche_Outline";
+    font-size: 40px;
+    color: ${({ theme }) => theme.color.purple};
+  }
+`;
