bad78a9b6dc8e2516297f1ff18482331022885b7
HyeJun <solar3070@naver.com>
2022-06-10 01:12:00 +0900

    design: 테마 적용

--- week7/src/App.js
+++ week7/src/App.js
@@ -1,12 +1,14 @@
 import Router from "./lib/router";
-import GlobalStyle from "./styles/global"
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ week7/src/styles/theme.js
@@ -0,0 +1,14 @@
+const color = {
+  ghostwhite: "#F9F8FE",
+  lightpurple: "#A895FF",
+  purple: "#8040FF",
+};
+
+const font = {};
+
+const theme = {
+  color,
+  font,
+};
+
+export default theme;
