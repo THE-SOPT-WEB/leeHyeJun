f556382e3cc925b0cdb406ebba2dd3e80b5d4956
HyeJun <solar3070@naver.com>
2022-06-21 20:06:49 +0900

    feat: postId 파라미터 props로 넘기기

--- week7/src/components/Edit/index.jsx
+++ week7/src/components/Edit/index.jsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import EditForm from "./EditForm";
+import { useParams } from "react-router-dom";
 
 function EditPage() {
+  const { postId } = useParams();
+
   return (
     <EditWrapper>
-      <EditForm />
+      <EditForm postId={postId} />
     </EditWrapper>
   );
 }
 
 export default EditPage;
 
-const EditWrapper = styled.div``;
+const EditWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
