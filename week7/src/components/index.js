cf2fc3865716c38bd8746aae987eae349b8cc390
HyeJun <solar3070@naver.com>
2022-06-21 20:52:58 +0900

    feat: components index 파일에 Write 추가

--- week7/src/components/index.js
+++ week7/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header/Header";
 export { default as Main } from "./Main/index";
+export { default as Write } from "./Write/index";
 export { default as Edit } from "./Edit/index";
