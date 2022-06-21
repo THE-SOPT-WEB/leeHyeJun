0634274a205ad17ad69ae34b9bbcc310eb1e5f13
HyeJun <solar3070@naver.com>
2022-06-21 20:51:42 +0900

    feat: 글 작성 폼 라우트 추가

--- week7/src/lib/router.jsx
+++ week7/src/lib/router.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { MainPage, EditPage } from "../pages";
+import { MainPage, WritePage, EditPage } from "../pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/write" element={<WritePage />} />
         <Route path="/edit/:postId" element={<EditPage />} />
       </Routes>
     </BrowserRouter>
